6a28ec6b55e9c55087bb224b2e48058c67e3e1b7
forgohill <forgohill@gmail.com>
2022-10-08 09:32:37 +0300

    9-32-08-10-2022

--- less1/js/14 MyFourteenProgram.js
+++ less1/js/14 MyFourteenProgram.js
@@ -7,7 +7,7 @@ while (num < 5) {
     num++;
 }
 
-let underscore = "_________________";
+let underscore = "_^_^_^_^_^_^_^_^_^_^_^_^_^_^_^_";
 console.log (underscore);
 
 let num1 = 5;
@@ -30,3 +30,44 @@ do {
 } while (num4 < 5); // повторяем цикл до тех пор пока num4 < 5
 console.log (underscore);
 
+for (let num5 = 0; num5 < 5; num5++ ){
+    console.log (num5);
+}
+console.log (underscore);
+
+//создание переменной снаружи цикла , а присвоение значения уже внутри цикла 
+// Это позволит работать с переменной не только внури цикла
+let num6;
+for (num6 = 0; num6 < 6; num6++){
+    console.log (num6)
+}
+console.log ('Вывод переменной вне цикла: ' + num6);
+console.log (underscore);
+
+// деректива break
+let num7 = 0;
+for ( ; num7 < 5; num7++) {
+    console.log (num7);
+    if (num7 == 2) break;
+}
+console.log ('Работа окончена num7 = ' + num7);
+console.log (underscore);
+
+// деректива coninue
+let num8 = 0;
+for (;num8 < 5;num8++) {
+    if (num8 == 2) continue;
+    console.log (num8)
+}
+console.log (underscore);
+
+//Метки
+first1For: for (let num9 = 0; num9 < 2; num9++) {
+    for (let size1 = 0; size1 < 3; size1++) {
+        if (size1 == 2) {
+            break first1For;
+        }
+        console.log (size1);
+    }
+}
+console.log (underscore);
