bc07af064084206d5a3c25a0fad6887d9155efc9
forgohill <forgohill@gmail.com>
2022-10-13 01:47:35 +0300

    1-47-13-10-22

--- less1/js/19 MyNineteenProgram.js
+++ less1/js/19 MyNineteenProgram.js
@@ -13,6 +13,7 @@ console.log (`Сумма: ${funcRezult1}`);
 
 
 // Рукурсия
+/*
 function calcSumm2(numOne2, numTwo2) {
     let result = 1;
         // умножается result на numOne2 numTwo2 раз в цикле
@@ -22,3 +23,47 @@ function calcSumm2(numOne2, numTwo2) {
     return result;
 }
 console.log (calcSumm2(2,3));
+*/
+function calcSumm2(numOne2,numTwo2) {
+    if(numTwo2 === 1) {
+        return numOne2;
+    } else {
+        return numOne2 * calcSumm2 ( numOne2, numTwo2 - 1 );
+    }
+}
+console.log (calcSumm2(2,2));
+
+// Функциональное выражение ( Function Expression)
+let showMessage = function () {
+    console.log ('привет!!!');
+};
+showMessage();
+
+function getSumm() {
+    let summ = 1 + 2;
+    console.log (summ);
+};
+let someVar = getSumm;
+
+someVar();
+getSumm();
+
+// Вытаскиеваем функцию за выражение при помощи Function Expression
+let getSumm2;
+if (2 > 1) {
+    getSumm2 = function () {
+        let summ2 = 1+2;
+        console.log (summ2);
+    };
+}
+getSumm2();
+
+// Однострочная стрелочная функция
+let getMessage = (text, name1) => text + ', ' + name1 + '!';
+console.log (getMessage('привет', 'Вася!!'));
+// Многострочная стрелочная функция
+let getMessage1 = (text, name2) => {
+    let message = text +', '+ name2 + '!';
+    return message;
+};
+console.log (getMessage1('Привет', 'Витёк'));
