2c0a09e6d7effc4bed74deb42ad19d709fe3e77e
forgohill <forgohill@gmail.com>
2022-10-23 01:33:46 +0300

    1 33 23 10 2022

--- less1/js/23 MyTwenteThreeProgram.js
+++ less1/js/23 MyTwenteThreeProgram.js
@@ -41,8 +41,10 @@ personObject[personProperty] = "Luisa"
 console.log(personObject);
 console.log("->-----------------4-");
 
+// программа которая выводит
+// значения свойств веведенных пользователем
 let myChair = {
-    "chashionMaterial": "",
+    "cushionMaterial": "",
     "numberOfLegs": "",
     "legHeight": "",
 };
@@ -67,3 +69,105 @@ configureChair();
 writeChairReceipt();
 
 console.log("->-----------------5-");
+
+// как удалять свойства из объекта
+let  myObjectDelete = {
+    var1: "the value",
+    var2: "another value",
+    var3: "yet another"
+};
+// удалить var2 из объекта myObjectDelete
+delete myObjectDelete.var2;
+// выводим в консоль myObjectDelete.var2
+// выводит undefined
+console.log(myObjectDelete.var2);
+console.log("->-----------------6-");
+
+// методы это занчения
+// свойств объектов 
+// которые являются функциями
+let sandwich ={
+    meat: "",
+    chees: "",
+    bread: "",
+    condiment: "",
+    makeSandwich: function (meat,cheese,bread,condiment) {
+        sandwich.meat = meat;
+        sandwich.chees = cheese;
+        sandwich.bread = bread;
+        sandwich.condiment = condiment;
+        let mySandwich = sandwich.bread + ", " + sandwich.meat + ", " + sandwich.chees + ", " + sandwich.condiment;
+        return mySandwich;
+    }
+};
+let sandwichOrder = sandwich.makeSandwich ("ham","cheaddar", "wheat","spicy mustard");
+document.write (sandwichOrder);
+console.log("->-----------------7-");
+
+
+// используем this
+// вместо конкретного объекта
+
+let sandwich1 ={
+    meat: "",
+    chees: "",
+    bread: "",
+    condiment: "",
+    makeSandwich: function (meat,cheese,bread,condiment) {
+        this.meat = meat;
+        this.chees = cheese;
+        this.bread = bread;
+        this.condiment = condiment;
+        let mySandwich = this.bread + ", " + this.meat + ", " + this.chees + ", " + this.condiment;
+        return mySandwich;
+    }
+};
+let sandwichOrder1 = sandwich1.makeSandwich ("ham1","cheaddar1", "wheat1","spicy mustard1");
+document.write (sandwichOrder1);
+console.log("->-----------------8-");
+
+// объектно-оринетрованный способ
+// наследование
+/*
+let pers = new Object();
+pers.eyes =2;
+pers.ears =2;
+pers.arms =2;
+pers.hands =2;
+pers.feet =2;
+pers.legs =2;
+pers.species ="Homo Sapiens";
+
+let willieNelson = new Object();
+willieNelson.eyes =2;
+willieNelson.ears =2;
+willieNelson.arms =2;
+willieNelson.hands =2;
+willieNelson.feet =2;
+willieNelson.legs =2;
+willieNelson.species ="Homo Sapiens";
+willieNelson.occupation ="musician";
+willieNelson.hometown ="Austin";
+willieNelson.hair ="Long";
+willieNelson.genre ="country";
+console.log(pers);
+console.log(willieNelson);
+
+* ниже будет приведет способ 
+* населодования для таких обектов
+* где тип объекта схож
+*/
+
+function Person1() {
+    this.eyes =2;
+    this.ears =2;
+    this.arms =2;
+    this.hands =2;
+    this.feet =2;
+    this.legs =2;
+    this.species ="Homo Sapiens";
+};
+let willieNelson = new Person1();
+console.log(willieNelson.feet);
+alert("Willie Nelson has " + willieNelson.feet + " feet!");
+console.log("->-----------------9-");
